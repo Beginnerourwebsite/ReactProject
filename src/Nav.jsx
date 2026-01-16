1a7ce891c77f14bced0b4d0ef6f1af6a60344126
PTC WebDevloper <ptc.services131@gmail.com>
2026-01-16 19:54:08 +0530

    16-01-2026

---
+++ src/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Nav() {
+	return (
+		<div>
+			<a href="/home">Home</a>
+			<br />
+			<a href="/Aboutus">Aboutus</a>
+			<br />
+			<a href="/Contactus">contact us</a>
+
+		</div>
+	)
+}
